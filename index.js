895046785168a04ada40e17403360fe6d36280f3
Andrey-Yartsev <andrey.yartsev.g@gmail.com>
2017-06-09 11:32:47 +0300

    fix readme

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
+if (!process.argv[2]) {
+  console.error('Syntax: node index.js db-name');
+  process.exit(1);
+}
+
+require('dotenv').config();
+
 const http = require('http');
-const dbName = 'chat-server';
+const fs = require('fs');
+const dbName = process.argv[2];
+const dbio = require('mongodb-io-native');
+const download = require('./download');
 
 let options = {
-  host: '95.79.46.186',
-  port: 8072,
-  // host: 'localhost',
-  // port: 3000,
+  host: process.env.SERVER_HOST,
+  port: process.env.SERVER_PORT,
   headers: {
     'Authorization': 'Bearer ' + process.env.SECRET
   }
@@ -28,44 +36,31 @@ const request = (path, onResponse) => {
   }).end();
 };
 
-const ObjectID = require('mongodb').ObjectID;
+const _request = (path, filePath, onResponse) => {
+  options.path = path;
+  download(options, filePath, onResponse);
+};
 
-require('mongodb').MongoClient.connect('mongodb://localhost:27017' +
-  '/' + dbName, function (err, db) {
-  console.log('db connected');
+request('/make-dump?dbName=' + dbName, (data) => {
+  data = JSON.parse(data);
+  if (data.error) {
+    console.error(data);
+    process.exit(1);
+  }
+  console.log('Downloading ' + data.file);
+  const filePath = __dirname + '/' + dbName + '.tgz';
 
-  request('/make-dump?dbName=' + dbName, (data) => {
-    data = JSON.parse(data);
-    if (data.error) {
-      console.error(data);
-      process.exit(1);
-    }
-    console.log('Downloading ' + data.file);
-    request(data.file, (data) => {
-      data = JSON.parse(data);
-      let importFunctions = [];
-      for (let v of data) {
-        ((collectionName, docs) => {
-          importFunctions.push(new Promise((resolve, reject) => {
-            db.collection(collectionName).remove({}, () => {
-              for (let i=0; i<docs.length; i++) {
-                docs[i]._id = ObjectID(docs[i]._id);
-              }
-              db.collection(collectionName).insertMany(docs, () => {
-                resolve(collectionName);
-              });
-            });
-          }));
-        })(v.collection, v.data);
-      }
-      Promise.all(importFunctions).then((r) => {
-        console.log('Done: ' + r);
+  _request(data.file, filePath, () => {
+    console.log('done');
+      dbio.import({
+        config: {
+          drop: true,
+          filePath
+        }, dbs: [dbName]
+      }).then(() => {
+        console.log('done');
         process.exit(0);
       });
-    });
   });
+
 });
-
-
-
-
